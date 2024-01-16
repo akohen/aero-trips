3cbeb9376aeb4e2585fe232d4de2e057ba0d9e87
Alexandre Kohen <alexandre@kohen.fr>
2024-01-16 20:05:54 +0100

    Add vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate', devOptions: { enabled: false } }),
+  ],
   base: "/aero-trips/",
   test: {
     globals: true,
