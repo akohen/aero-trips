8430c88260cfd3fd7a527d23f153938026defa45
Alexandre Kohen <alexandre@kohen.fr>
2024-01-16 08:57:24 +0100

    Add search to airfields list

--- src/routes/FieldsPage.tsx
+++ src/routes/FieldsPage.tsx
@@ -1,8 +1,9 @@
 
-import { Table, Pagination, Center } from '@mantine/core';
+import { Table, Pagination, Center, TextInput, rem, Text } from '@mantine/core';
 import airfields from '../data/airfields.json'
 import airac from '../data/airac';
 import { useState } from 'react';
+import { IconSearch } from '@tabler/icons-react';
 
 function chunk<T>(array: T[], size: number): T[][] {
   if (!array.length) {
@@ -13,11 +14,29 @@ function chunk<T>(array: T[], size: number): T[][] {
   return [head, ...chunk(tail, size)];
 }
 
-const data = chunk(Object.values(airfields),20);
+const data = Object.values(airfields);
+let chunkedData = chunk(data, 20);
 
 function FieldsPage() {
+  const [search, setSearch] = useState('');
   const [activePage, setPage] = useState(1);
-  const rows = data[activePage-1].map(e => (
+  
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    setSearch(value);
+    chunkedData = chunk(filterData(data, value),20);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function filterData(data: any[], search: string) {
+    const query = search.toLowerCase().trim();
+    return data.filter((item) =>
+      ['codeIcao', 'name'].some((key) => item[key].toLowerCase().includes(query.normalize("NFD").replace(/\p{Diacritic}/gu, "")))
+    );
+  }
+
+  const rows = chunkedData.length > 0 ? chunkedData[activePage-1].map(e => (
     <Table.Tr key={e.codeIcao}>
       <Table.Td>{e.name}</Table.Td>
       <Table.Td>{e.codeIcao}</Table.Td>
@@ -27,7 +46,7 @@ function FieldsPage() {
         </a>
       </Table.Td>
     </Table.Tr>
-  ));
+  )) : [];
   
     return (<>
     <Table stickyHeader stickyHeaderOffset={60} highlightOnHover>
@@ -38,9 +57,27 @@ function FieldsPage() {
           <Table.Th>Carte VAC</Table.Th>
         </Table.Tr>
       </Table.Thead>
-      <Table.Tbody>{rows}</Table.Tbody>
+      <Table.Tbody>{rows.length > 0 ? (
+            rows
+          ) : (
+            <Table.Tr>
+              <Table.Td colSpan={Object.keys(data[0]).length}>
+                <Text fw={500} ta="center">
+                  Aucun résultat
+                </Text>
+              </Table.Td>
+            </Table.Tr>
+          )}</Table.Tbody>
     </Table>
-    <Center><Pagination className="paginationtest" total={data.length} value={activePage} onChange={setPage} mt="sm" /></Center>
+    <Center>
+    <TextInput
+        placeholder="Chercher un terrain"
+        leftSection={<IconSearch style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
+        value={search}
+        onChange={handleSearchChange}
+      />
+      <Pagination className="paginationtest" total={data.length} value={activePage} onChange={setPage} />
+    </Center>
     </>
     )
   }
