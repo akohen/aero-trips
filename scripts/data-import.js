71577bfb82e73033d5535bb08250a993dca8ede7
Alexandre KOHEN <alexandre@kohen.fr>
2024-01-17 17:02:53 +0000

    Start admin import of airfields

--- scripts/data-import.js
+++ scripts/data-import.js
@@ -1,5 +1,8 @@
 import { XMLParser } from "fast-xml-parser";
 import { readFileSync, writeFileSync } from "fs";
+import admin from "firebase-admin";
+import { exit } from "process";
+
 
 function DMS2DD(str) {
     const S = parseFloat(str)%100
@@ -8,6 +11,28 @@ function DMS2DD(str) {
     return (D+(M+S/60)/60) * ('SW'.includes(str[str.length-1]) ? -1 : 1)
 }
 
+import creds from "../serviceAccountKey.json" with { "type": "json" }
+const firebaseConfig = {
+  apiKey: "AIzaSyAleHj_gty6XncQLEDlLn3Ih7X08KuQ-jw",
+  authDomain: "aero-trips.firebaseapp.com",
+  projectId: "aero-trips",
+  storageBucket: "aero-trips.appspot.com",
+  messagingSenderId: "484361364174",
+  appId: "1:484361364174:web:4c4eaf632f931956aca69f",
+  measurementId: "G-CKJYT103VV",
+  credential: admin.credential.cert(creds),
+};
+
+admin.initializeApp(firebaseConfig);
+const db = admin.firestore();
+const res = await db.doc(`airfields/XXXX`).set({name:"foo"});
+//await setDoc(doc(db, "airfields", "XXXX"), {name:"test"})
+//const db = getFirestore(app);
+//const db = initializeFirestore(app)
+
+
+
+exit(0)
 const xmlFile = readFileSync(`./scripts/AIXM4.5_all_FR_OM_2023-10-05.xml`, 'utf8');
 const parser = new XMLParser();
 let jsonObj = parser.parse(xmlFile);
