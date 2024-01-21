cc979702312e753da71009e0cb174ed9545cadc0
Alexandre Kohen <alexandre@kohen.fr>
2024-01-21 07:13:57 +0100

    Fix ActivityDetails component name

--- src/routes/ActivityDetails.tsx
+++ src/routes/ActivityDetails.tsx
@@ -1,13 +1,14 @@
 import { useParams } from "react-router-dom";
 import { Activity } from "../types";
 
-const AirfieldDetails = ({activities} : {activities:Map<string,Activity>}) => {
+const ActivityDetails = ({activities} : {activities:Map<string,Activity>}) => {
   const params = useParams();
   const activity = params.activityId ? activities.get(params.activityId) : undefined;
   return activities.size > 0 ? activity ? (<>
     <h1>Fiche {activity.name}</h1>
     <p>{activity.description}</p>
     <p>{activity.type.map(t => (<span key={t}>{t} </span> ))}</p>
+    
   </>) : (
     <p>Activité/lieu inconnu</p>
   ) : (
@@ -15,4 +16,4 @@ const AirfieldDetails = ({activities} : {activities:Map<string,Activity>}) => {
   )
 }
 
-export default AirfieldDetails
+export default ActivityDetails
