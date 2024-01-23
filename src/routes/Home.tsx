86520bc216bf40dd1ed80a996a98db8ea8f314da
Alexandre Kohen <alexandre@kohen.fr>
2024-01-23 18:25:12 +0100

    Quick homepage text

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,22 +1,15 @@
-import { useState } from 'react'
-import { Button } from '@mantine/core';
 
 const Home = () => {
-  const [count, setCount] = useState(0)
-
   return (<>
-  <h1>Vite + React</h1>
+  <h1>Bienvenue sur Aero trips</h1>
   <div className="card">
-  <Button onClick={() => setCount((count) => count + 1)}>
-      count is {count}
-  </Button>
   <p>
-      Edit <code>src/App.tsx</code> and save to test HMR
+      Le but de ce site est d'aider les pilotes privés à trouver des idées de sorties en avion ainsi que des activités à proximité des aérodromes.
+  </p>
+  <p>
+    Ne site n'est pas une source d'information aéronautique, et ne peut pas se substituer à la documentation réglementaire lors de la préparation d'un vol.
   </p>
   </div>
-  <p className="read-the-docs">
-  Click on the Vite and React logos to learn more
-  </p>
 </>)}
 
 export default Home
