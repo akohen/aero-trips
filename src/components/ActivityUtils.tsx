81d8f46861d00f9069b7f913a2452792eb79662e
Alexandre Kohen <alexandre@kohen.fr>
2024-01-27 10:56:07 +0100

    Add tooltip + key to activity icons

--- src/components/ActivityUtils.tsx
+++ src/components/ActivityUtils.tsx
@@ -1,17 +1,22 @@
-import { Text } from "@mantine/core";
+import { Text, Tooltip } from "@mantine/core";
 import { Activity } from "..";
 import { IconBed, IconBulb, IconBus, IconEye, IconSoup } from "@tabler/icons-react";
 
+const iconStyle = {
+  size:16,
+  style:{verticalAlign:'middle'}
+}
+
 const icons = {
-  food: <IconSoup size={16} style={{verticalAlign:'middle'}} />,
-  transport: <IconBus size={16} style={{verticalAlign:'middle'}} />,
-  lodging: <IconBed size={16} style={{verticalAlign:'middle'}} />,
-  poi: <IconEye size={16} style={{verticalAlign:'middle'}} />,
-  other: <IconBulb size={16} style={{verticalAlign:'middle'}} />,
+  food: (i: number) => <Tooltip key={i} label="Restauration"><IconSoup {...iconStyle} /></Tooltip>,
+  transport: (i: number) => <Tooltip key={i} label="Transport"><IconBus key={i} {...iconStyle} /></Tooltip>,
+  lodging: (i: number) => <Tooltip key={i} label="Hébergement"><IconBed key={i} {...iconStyle} /></Tooltip>,
+  poi: (i: number) => <Tooltip key={i} label="A voir du ciel"><IconEye key={i} {...iconStyle} /></Tooltip>,
+  other: (i: number) => <Tooltip key={i} label="Autre activité"><IconBulb key={i} {...iconStyle} /></Tooltip>,
 }
 
 export const ActivityTitle = ({activity}: {activity: Activity}) => (
 <Text size="sm" className="list-item-title">
   {activity.name}
-  {activity.type.map(e => (icons[e]))}
+  {activity.type.map((e,i) => icons[e](i))}
 </Text>)
