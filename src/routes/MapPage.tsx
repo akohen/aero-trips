004c5ba5fa9056c0336cf5e54e953d437aef8dbf
Alexandre Kohen <alexandre@kohen.fr>
2024-01-28 19:18:10 +0100

    Add Activities filter to map page

--- src/routes/MapPage.tsx
+++ src/routes/MapPage.tsx
@@ -1,17 +1,17 @@
 import { MapContainer, TileLayer } from 'react-leaflet'
-import { Activity, Airfield, ADfilter } from '..';
+import { Activity, Airfield, ADfilter, ActivityFilter } from '..';
 import { useParams } from 'react-router-dom';
 import { LatLngExpression } from 'leaflet';
-import { filterAirfields } from '../utils';
+import { filterActivities, filterAirfields } from '../utils';
 import { AirfieldMarker } from '../components/AirfieldUtils';
 import { ActivityMarker } from '../components/ActivityUtils';
 
-function MapPage({airfields, activities, ADfilter} : {airfields: Map<string,Airfield>, activities: Map<string,Activity>, ADfilter: ADfilter}) {
+function MapPage({airfields, activities, ADfilter, ActFilter} : {airfields: Map<string,Airfield>, activities: Map<string,Activity>, ADfilter: ADfilter, ActFilter:ActivityFilter}) {
   const params = useParams();
   const center: LatLngExpression = (params.lat && params.lng) ? [parseFloat(params.lat), parseFloat(params.lng)] : [48.81,2.06]
   const airfieldsMarkers = [...filterAirfields(airfields, activities, ADfilter)].map( ([key,e]) => <AirfieldMarker key={key} id={key} airfield={e} />);
 
-  const activitiesMarkers = [...activities].map( ([key,e]) => <ActivityMarker key={key} id={key} activity={e} />);
+  const activitiesMarkers = [...filterActivities(activities, ActFilter)].map( ([key,e]) => <ActivityMarker key={key} id={key} activity={e} />);
 
   return (<div>
     <MapContainer style={{ height: "700px" }} center={center} zoom={10} scrollWheelZoom={true} >
