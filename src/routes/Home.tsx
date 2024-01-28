ed242bc560e5ee0e8727c5d73a76695911812e02
Alexandre Kohen <alexandre@kohen.fr>
2024-01-28 20:41:42 +0100

    Homepage text update

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 const Home = () => {
   return (<>
@@ -5,6 +6,13 @@ const Home = () => {
   <div className="card">
     <p>
       Le but de ce site est d'aider les pilotes privés à trouver des idées de sorties en avion ainsi que des activités à proximité des aérodromes.
+    </p>
+    <p>
+      Utilisez les filtres sur la liste des <Link to={'/airfields'}>terrains</Link> ou des <Link to={'/activities'}>activités</Link> pour choisir les éléments qui sont affichés sur la <Link to={'/map'}>carte</Link>.
+    </p>
+    <p>
+      Les données de ce site sont collaboratives, n'hésitez donc à pas à rajouter des lieux ou des propositions de sorties, ou à corriger les informations déjà en ligne. 
+      Les modifications étant validées manuellement, votre modification n'apparaîtra pas immédiatement sur le site.
     </p>
     <p>
       Ce site n'est pas une source d'information aéronautique, et ne peut pas se substituer à la documentation réglementaire lors de la préparation d'un vol.
