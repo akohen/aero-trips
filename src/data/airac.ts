7d7b60bc5676427437860aeb809455b5ef09d430
Alexandre Kohen <alexandre@kohen.fr>
2024-04-19 17:58:32 +0200

    Fix airac months names

--- src/data/airac.ts
+++ src/data/airac.ts
@@ -1,7 +1,7 @@
 export function getAirac(reference: Date = new Date()) {
   let current = new Date("2023-12-28T10:00Z")
   while (nextAirac(current) < reference) { current = nextAirac(current) }
-  const MONTHS = ['JAN', 'FEB', 'MAR', 'AVR', 'MAI', 'JUN', 'JUI', 'AOU', 'SEP', 'OCT', 'NOV', 'DEC']
+  const MONTHS = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
   return String(current.getDate()).padStart(2,'0') + '_' + MONTHS[current.getMonth()] + '_' + current.getFullYear()
 }
 
