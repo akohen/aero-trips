6cd61b5b0c19e138cc51a33512b54fc140013840
Alexandre Kohen <alexandre@kohen.fr>
2024-07-07 07:43:46 +0200

    Change homepage text

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,28 +1,42 @@
-import { Text, Title } from "@mantine/core"
+import { Paper, Text, Title } from "@mantine/core"
 import { Link } from "react-router-dom"
 
 const Home = () => {
   return (<>
   <Title>Bienvenue sur Aero trips</Title>
-  <div className="card">
-    <p>
-      Le but de ce site est d'aider les pilotes privés à trouver des idées de sorties en avion ainsi que des activités à proximité des aérodromes.
-    </p>
-    <p>
-      Utilisez les filtres sur la liste des <Link to={'/airfields'}>terrains</Link> ou des <Link to={'/activities'}>activités</Link> pour choisir les éléments qui sont affichés sur la <Link to={'/map'}>carte</Link>.
-    </p>
-    <p>
+  <Paper
+    shadow="md"
+    radius="md"
+    p='sm'
+    mt={"md"}
+  >
+    <Text>
+      Aerotrips est un site <b>gratuit</b>, <b>collaboratif</b> et <b>open-source</b>&nbsp;
+      pour aider les pilotes privés à partager des idées de sorties en avion,
+      ainsi que des activités à proximité des aérodromes.
+    </Text>
+    <Text>
       Les données de ce site sont collaboratives, n'hésitez donc à pas à rajouter des lieux ou des propositions de sorties, ou à corriger les informations déjà en ligne. 
-      Les modifications étant validées manuellement, votre modification n'apparaîtra pas immédiatement sur le site.
-    </p>
-    <p>
-      Le nom définitif du projet est encore à définir, n'hésitez pas à soumettre vos propositions de nom.
-    </p>
-    <p>
+      Les modifications des visiteurs anonymes sont validées manuellement, votre modification n'apparaîtra pas immédiatement sur le site.
+    </Text>
+    <Text>
+      Vous pouvez vous connecter (uniquement avec un compte Google pour le moment) 
+      afin de pouvoir:
+    </Text>
+    <ul>
+        <li>Valider automatiquement vos changements sur des activités</li>
+        <li>Partager des voyages</li>
+        <li>Enregistrer votre aérodrome de base</li>
+        <li>Lister les terrains que vous avez visités (bientôt)</li>
+      </ul>
+    <Text>
+      Utilisez les filtres sur la liste des <Link to={'/airfields'}>terrains</Link> ou des <Link to={'/activities'}>activités</Link> pour choisir les éléments qui sont affichés sur la <Link to={'/map'}>carte</Link>.
+    </Text>
+    <Text mt={"md"}>
       ⚠ Ce site n'est pas une source d'information aéronautique, et ne peut pas se substituer à la documentation réglementaire lors de la préparation d'un vol.
-    </p>
-    <Text size="sm">Aéro trips version {APP_VERSION}</Text>
-  </div>
+    </Text>
+    <Text size="sm" ta={"right"}>Aéro trips version {APP_VERSION}</Text>
+  </Paper>
 </>)}
 
 export default Home
