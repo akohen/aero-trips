76374a2631d2a4984fb60982618a0aec677a30a9
Alexandre KOHEN <alexandre@kohen.fr>
2024-08-12 16:36:37 +0000

    Add some default images for ActivityCard

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -1,11 +1,11 @@
 import { Button, Group, Paper, Stack, Text } from '@mantine/core';
-import { Activity } from '..';
+import { Activity, ActivityType } from '..';
 import { Link } from 'react-router-dom';
 import { CommonIcon } from './CommonIcon';
 
 const ActivityCard = ({id, activity, distance}: {id:string, activity:Activity, distance?:number}) => {
     const imgNode = activity.description?.content != undefined ? activity.description.content.find( (a: { type: string }) => a.type =='image') : undefined
-    const imgURL = imgNode ? imgNode.attrs.src : 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/991px-Placeholder_view_vector.svg.png'
+    const imgURL = getImage(imgNode, activity.type)
     return (
         <Paper
             shadow="md"
@@ -42,5 +42,11 @@ const ActivityCard = ({id, activity, distance}: {id:string, activity:Activity, d
     );
 }
 
-
+const getImage = (imgNode:{attrs:{src:string}}, types:ActivityType[]) => {
+    if(imgNode) return imgNode.attrs.src
+    if(types.includes('food')) return 'https://firebasestorage.googleapis.com/v0/b/aero-trips.appspot.com/o/images%2Frestaurant.jpg?alt=media&token=257ab852-a209-414d-8d77-671d91ce3aa7'
+    if(types.includes('lodging')) return 'https://firebasestorage.googleapis.com/v0/b/aero-trips.appspot.com/o/images%2Flodging.jpg?alt=media&token=93c51d40-d215-4fa2-b69c-51162fe5717d'
+    if(types.includes('transit')) return 'https://firebasestorage.googleapis.com/v0/b/aero-trips.appspot.com/o/images%2Ftransit.jpg?alt=media&token=375e623c-aec6-4e51-a888-cfd38019a7d0'
+    return 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/991px-Placeholder_view_vector.svg.png'
+}
 export default ActivityCard
