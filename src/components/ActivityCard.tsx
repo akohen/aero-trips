bcca567e28c960db734cfef218e84114a68bb6a1
Alexandre KOHEN <alexandre@kohen.fr>
2024-08-17 09:59:50 +0000

    Add default hiking image

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -49,6 +49,7 @@ const getImage = (imgNode:{attrs:{src:string}}, types:ActivityType[]) => {
     if(types.includes('transit')) return 'https://firebasestorage.googleapis.com/v0/b/aero-trips.appspot.com/o/images%2Ftransit.jpg?alt=media&token=375e623c-aec6-4e51-a888-cfd38019a7d0'
     if(types.includes('bike')) return 'https://firebasestorage.googleapis.com/v0/b/aero-trips.appspot.com/o/images%2Fbike.jpg?alt=media&token=161bd4af-01b7-4be5-b3f9-c4bd22aca5db'
     if(types.includes('car')) return 'https://firebasestorage.googleapis.com/v0/b/aero-trips.appspot.com/o/images%2Fcar.jpg?alt=media&token=3b0bfb95-b994-487c-8552-fd88e5b7fccc'
+    if(types.includes('hiking')) return 'https://firebasestorage.googleapis.com/v0/b/aero-trips.appspot.com/o/images%2Fhiking.jpg?alt=media&token=942cf1d2-1501-4524-bc10-c2fe425253e8'
     if(types.includes('nautical')) return 'https://firebasestorage.googleapis.com/v0/b/aero-trips.appspot.com/o/images%2Fnautical.jpg?alt=media&token=7d117c7e-e1d9-4787-9ec6-95064a1d5f94'
     return 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/991px-Placeholder_view_vector.svg.png'
 }
