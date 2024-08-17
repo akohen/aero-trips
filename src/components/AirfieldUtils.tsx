7ad3753febc5e5f91da1a731869e18349bc12bb7
Alexandre KOHEN <alexandre@kohen.fr>
2024-08-17 13:34:04 +0000

    Remove Toilet icon from Airfield title

--- src/components/AirfieldUtils.tsx
+++ src/components/AirfieldUtils.tsx
@@ -8,7 +8,7 @@ import pinRunway from '/map-pin-runway.svg'
 
 
 export const AirfieldTitle = ({ad}: {ad: Airfield}) => {
-  return (<><CommonIcon iconType={ad.status} /> {ad.name} {ad.fuels?.map(e => <CommonIcon key={e} iconType={e} />)}<CommonIcon iconType={ad.toilet||''} /></>)
+  return (<><CommonIcon iconType={ad.status} /> {ad.name} {ad.fuels?.map(e => <CommonIcon key={e} iconType={e} />)}</>)
 }
 
 export const NearbyAirfields = ({items} : {items: [distance: number, item: Airfield, id: string][]}) => {
