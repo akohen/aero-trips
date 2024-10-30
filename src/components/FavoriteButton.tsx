3a095a564fa40d6433f67d17d5d7bfe50de63447
Alexandre KOHEN <alexandre@kohen.fr>
2024-10-30 15:59:53 +0000

    Moved favorite functions to FavoriteButton

--- src/components/FavoriteButton.tsx
+++ src/components/FavoriteButton.tsx
@@ -1,13 +1,21 @@
 import { Airfield, Profile } from ".."
-import { addFavorite, removeFavorite } from "../data/profileUtils"
 import { Text } from "@mantine/core"
 import { IconHeart, IconHeartFilled } from "@tabler/icons-react"
 
 const FavoriteButton = ({item, profile} : {item: Airfield, profile: Profile}) => {
   return (profile?.favorites?.includes(item.codeIcao) ? 
-  <Text onClick={() => removeFavorite(profile, item.codeIcao)}>Retirer des favoris<IconHeartFilled size={20} /></Text>
+    <Text onClick={() => 
+      profile.update({favorites: profile.favorites?.filter(e => e != item.codeIcao)})
+    }>
+      Retirer des favoris<IconHeartFilled size={20} />
+    </Text>
   : 
-  <Text onClick={() => addFavorite(profile, item.codeIcao)}>Ajouter aux favoris<IconHeart size={20} /></Text>)
+    <Text onClick={() => 
+      profile.update({favorites:[...new Set((profile.favorites ?? []).concat([item.codeIcao]))]})
+    }>
+      Ajouter aux favoris<IconHeart size={20} />
+    </Text>
+  )
 }
 
 export default FavoriteButton
