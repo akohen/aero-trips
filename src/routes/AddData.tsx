c42af032961c240e186e575f15c706f44c5a8a18
Alexandre Kohen <alexandre@kohen.fr>
2024-11-03 17:59:00 +0100

    Update add page text

--- src/routes/AddData.tsx
+++ src/routes/AddData.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Activity, Airfield, Data, Trip } from "..";
-import { Button, Group, Paper } from "@mantine/core";
+import { Button, Group, Paper, Title } from "@mantine/core";
 import { IconMapRoute, IconBulb } from "@tabler/icons-react";
 import ActivityForm from "../components/ActivityForm";
 import { GeoPoint } from "firebase/firestore";
 import AirfieldForm from "../components/AirfieldForm";
 import TripForm from "../components/TripForm";
+import BackButton from "../components/BackButton";
 
 const AddData = (data: Data) => {
   const params = useParams();
@@ -34,16 +35,31 @@ const AddData = (data: Data) => {
   || type == 'trips' && (<TripForm id={params.id} {...data} />) 
   || type == 'airfields' && (<AirfieldForm airfield={entity as Airfield} profile={data.profile} />) 
   : (
-    <Paper>
-      <Group justify="center">
-        <p>Proposer</p>
+    <>
+    <Title><BackButton />Contribuer</Title>
+    <Paper
+      shadow="md"
+      radius="md"
+      p='sm'
+      mt="md"
+      withBorder
+    >
+      
+        <p>Merci de contribuer à améliorer les données du site!</p>
+        <p>Plusieurs types de données peuvent être modifiées:</p>
+
+        <ul>
+          <li><b>Les terrains d'aviation</b>: Les données de base sont importées à partir des exports du SIA, pour rajouter des détails ou des commentaires sur un terrain, allez sur la fiche de ce terrain et cliquez sur le bouton "modifier" en haut.</li>
+          <li><b>Les activités</b>: Pour ajouter une activité, faites un clic-droit sur la carte à l'endroit où vous souhaitez ajouter l'activité. Pour modifier une activité, allez sur sa fiche et cliquez sur le bouton "modifier" en haut.</li>
+          <li><b>Les sorties</b>: Pour ajouter une sortie, cliquez sur le bouton ci-dessous. Seule la personne ayant proposé une sortie peut la modifier.</li>
+        </ul>
+      <Group>
         <Button leftSection={<IconBulb size={14} />} onClick={() => setType('activities')}>Lieu ou activité</Button>
         <Button leftSection={<IconMapRoute size={14} />} onClick={() => setType('trips')}>Sortie</Button>
       </Group>
-      <Group justify="center">
-        <p>Il est également possible d'ajouter des lieux ou activités depuis la carte, en faisant un clic droit à l'emplacement où ajouter ce nouveau lieu.</p>
-      </Group>
-    </Paper>
+      <p>En cas de problème ou de questions, n'hésitez pas à <Link to={'/contact'}>nous contacter directement</Link>.</p>
+
+    </Paper></>
   )
 }
 
