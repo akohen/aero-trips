b36717e7a1d990100cc664a12a6a3d06d46361aa
Alexandre KOHEN <alexandre@kohen.fr>
2024-12-28 09:47:06 +0000

    Add link to contact form in anonymous submission

--- src/components/AnonymousSubmission.tsx
+++ src/components/AnonymousSubmission.tsx
@@ -15,6 +15,7 @@ const AnonymousSubmission = ({activity} : {activity?: Activity}) => {
       <p>Vos modifications ont été enregistées, et seront visibles sur le site d'ici quelques jours (pour modération).</p>
       <p>Si vous souhaitez proposer plusieurs modifications, n'hésitez pas à vous connecter, 
         car les modifications des utilisateurs connectés sont modérées à posteriori, et sont donc immédiatement visibles.</p>
+      <p>Si vous avez des problèmes ou des questions, n'hésitez pas à <Link to="/contact">nous contacter</Link>.</p>
       <p><Link to=".." relative="path">Retour</Link></p>
     </>
   )}
