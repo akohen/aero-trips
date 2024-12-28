f00edf28a79f051ca4f6b4095a1a6fc4a206e2bb
Alexandre KOHEN <alexandre@kohen.fr>
2024-12-28 08:47:53 +0000

    Synchronize page number with query parameters
    This allows returning to the same page when navigating back to the list.
    This will then be used for filters so that a search can be shared.
    (might need to update the setSearchParams call)

--- src/components/TableList.tsx
+++ src/components/TableList.tsx
@@ -1,5 +1,6 @@
 import { Table, Pagination, Center, Text } from '@mantine/core';
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 function chunk<T>(array: T[], size: number): T[][] {
   if (!array.length) {return [];}
@@ -10,13 +11,21 @@ function chunk<T>(array: T[], size: number): T[][] {
 
 function TableList<T>( {data, row, empty, columns} : 
     {data: Map<string,T>, row:(arg0: [string,T], arg1: number)=>JSX.Element, empty: JSX.Element, columns: string[]} ) {
-
-  const [activePage, setPage] = useState(1);
+  
+  const [searchParams, setSearchParams] = useSearchParams()
+  const [activePage, setPage] = useState(Number(searchParams.get("page")) || 1);
   const chunks = chunk([...data],15)
+  const updatePage = (page: number) => {
+    if(data.size === 0) return // Wait for data to be loaded before resetting to page 1
+    const pageId = Math.max(1, Math.min(page, chunks.length))
+    setPage(pageId)
+    setSearchParams({page: pageId.toString()})
+  }
+
   useEffect(() => {
-    const page = Math.min(activePage, chunks.length)
-    setPage(Math.max(1, page))
-  }, [activePage, chunks])
+    updatePage(activePage)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [chunks.length]) // This is to ensure that the active page does not exceed the number of pages when filters are changed
 
   const rows = chunks.length > 0 ? chunks[activePage-1]?.map(row) : (
   <Table.Tr>
@@ -35,7 +44,7 @@ function TableList<T>( {data, row, empty, columns} :
       </Table.Thead>
       <Table.Tbody>{rows}</Table.Tbody>
     </Table>
-    <Center><Pagination total={chunks.length} value={activePage} onChange={setPage} />{data.size > 0 && <Text ml={'md'}>({data.size} résultats)</Text>}</Center>
+    <Center><Pagination total={chunks.length} value={activePage} onChange={updatePage} />{data.size > 0 && <Text ml={'md'}>({data.size} résultats)</Text>}</Center>
   </>)
 }
 
