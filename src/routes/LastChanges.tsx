b4556902da29a8ce24cde9071c0c9ac6cd70c3f2
Alexandre KOHEN <alexandre@kohen.fr>
2025-07-31 11:51:46 +0000

    Fix links in LastChanges list

--- src/routes/LastChanges.tsx
+++ src/routes/LastChanges.tsx
@@ -1,7 +1,6 @@
 
 import { Table, Title } from '@mantine/core';
 import { Activity, Airfield, Data } from '..';
-import { useNavigate } from 'react-router-dom';
 import List from '../components/TableList';
 import { ButtonViewOnMap } from '../components/CommonButtons';
 import { ActivityTitle } from '../components/ActivityUtils';
@@ -16,11 +15,6 @@ function LastChanges({airfields, activities, setMapView, profile} : Data) {
       ([, e]) => e.updated_at && (e.updated_at.seconds * 1000) > (Date.now() - 1000 * 60 * 60 * 24 * 30)
     );
   const data: Map<string, Activity | Airfield> = new Map(items);
-  const navigate = useNavigate();
-  const clickable = (e: Activity | Airfield) => ({
-    className: 'clickable',
-    onClick: () => navigate('codeIcao' in e ? `/airfields/${e.codeIcao}` : `/activities/${e.id}`)
-  });
   
   return (<>
     <Title order={2}>Dernières modifications</Title>
@@ -31,16 +25,18 @@ function LastChanges({airfields, activities, setMapView, profile} : Data) {
       columns={[
         {
           title:"Nom",
+          linkTo: (e) => 'codeIcao' in e ? `/airfields/${e.codeIcao}` : `/activities/${e.id}`,
           row: (e) => (
-            <Table.Td {...clickable(e)}>
+            <Table.Td>
               {'codeIcao' in e ? <AirfieldTitle ad={e as Airfield} profile={profile} /> : <ActivityTitle activity={e as Activity} profile={profile} />}
             </Table.Td>
           )
         },
         {
           title:'Dernière modification', 
+          linkTo: (e) => 'codeIcao' in e ? `/airfields/${e.codeIcao}` : `/activities/${e.id}`,
           sortFn: (a: Activity | Airfield, b: Activity | Airfield) => a.updated_at!.seconds - b.updated_at!.seconds,
-          row: (e) => <Table.Td {...clickable(e)}>{formatDate(e.updated_at!)}</Table.Td>
+          row: (e) => <Table.Td>{formatDate(e.updated_at!)}</Table.Td>
         },
         {
           row: e => (<Table.Td align='right'><ButtonViewOnMap item={e} setMapView={setMapView} compact/></Table.Td>)
