e62759ab15389a3d9ad588611f4988fc065b2123
Alexandre KOHEN <alexandre@kohen.fr>
2025-07-31 16:32:19 +0000

    Small buttons (without text) on mobile on lists

--- src/components/CommonButtons.tsx
+++ src/components/CommonButtons.tsx
@@ -1,31 +1,36 @@
-import { Button } from "@mantine/core";
+import { Button, em } from "@mantine/core";
 import { IconMapCheck, IconMapPinSearch } from "@tabler/icons-react";
 import { GeoPoint } from "firebase/firestore";
 import { Link } from "react-router-dom";
 import { getVacUrl } from "../data/airac";
 import { Airfield, MapView } from "..";
+import { useMediaQuery } from "@mantine/hooks";
 
-export const ButtonViewOnMap = ({item, setMapView, compact}:{item:{position:GeoPoint}, compact?: boolean, setMapView: (v:MapView) => void}) => (
+export const ButtonViewOnMap = ({item, setMapView, compact}:{item:{position:GeoPoint}, compact?: boolean, setMapView: (v:MapView) => void}) => {
+  const isMobile = useMediaQuery(`(max-width: ${em(768)})`);
+  return (
   <Button
     component={Link}
     to={`/map`}
     size={compact ? "compact-sm" : 'sm'}
-    leftSection={<IconMapPinSearch size={20} />}
+    leftSection={isMobile ? undefined : <IconMapPinSearch size={20} />}
     onClick={() => setMapView({center:[item.position.latitude, item.position.longitude], zoom: 12})}
   >
-    Voir sur la carte
+    {isMobile ? <IconMapPinSearch size={20} /> : 'Voir sur la carte'}
   </Button>
-)
+)}
 
-export const ButtonVACMap = ({airfield, compact}: {airfield:Airfield, compact?:boolean}) => (
+export const ButtonVACMap = ({airfield, compact}: {airfield:Airfield, compact?:boolean}) => {
+  const isMobile = useMediaQuery(`(max-width: ${em(768)})`);
+  return (
   <Button
     component={Link}
     to={getVacUrl(airfield.codeIcao)}
     target='_blank'
     size={compact ? "compact-sm" : 'sm'}
-    leftSection={<IconMapCheck size={20} />}
+    leftSection={isMobile ? undefined : <IconMapCheck size={20} />}
     title={`Voir la carte VAC de ${airfield.codeIcao} - ${airfield.name} dans un nouvel onglet`}
   >
-    Carte VAC
+    {isMobile ? <IconMapCheck size={20} /> : 'Carte VAC'}
   </Button>
-)
+)}
