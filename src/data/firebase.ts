cbe2a6272d7e4b894b8661ee68e8824f932af50b
Alexandre KOHEN <alexandre@kohen.fr>
2026-04-15 11:55:03 +0200

    Data update

--- src/data/firebase.ts
+++ src/data/firebase.ts
@@ -1,25 +1,31 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
-import { FirebaseOptions, initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { initializeFirestore } from "firebase/firestore";
 
-export const firebaseConfig: FirebaseOptions = import.meta.env.PROD ? {
-  apiKey: "AIzaSyAleHj_gty6XncQLEDlLn3Ih7X08KuQ-jw",
-  authDomain: "aerotrips.fr",
-  projectId: "aero-trips",
-  storageBucket: "aero-trips.appspot.com",
-  messagingSenderId: "484361364174",
-  appId: "1:484361364174:web:4c4eaf632f931956aca69f",
-  measurementId: "G-CKJYT103VV"
-} : {
-  apiKey: "AIzaSyBdEJDEfGkpc2m6s-N0hW1jP-o1vNdOGHE",
-  authDomain: "aero-trips-staging.firebaseapp.com",
-  projectId: "aero-trips-staging",
-  storageBucket: "aero-trips-staging.appspot.com",
-  messagingSenderId: "39898345407",
-  appId: "1:39898345407:web:7cdab41986edf28898bb0a"
-};
+export const firebaseConfig = {
+  'production': {
+    apiKey: "AIzaSyAleHj_gty6XncQLEDlLn3Ih7X08KuQ-jw",
+    authDomain: "aerotrips.fr",
+    projectId: "aero-trips",
+    storageBucket: "aero-trips.appspot.com",
+    messagingSenderId: "484361364174",
+    appId: "1:484361364174:web:4c4eaf632f931956aca69f",
+    measurementId: "G-CKJYT103VV"
+  },
+  'staging': {
+    apiKey: "AIzaSyBdEJDEfGkpc2m6s-N0hW1jP-o1vNdOGHE",
+    authDomain: "aero-trips-staging.firebaseapp.com",
+    projectId: "aero-trips-staging",
+    storageBucket: "aero-trips-staging.appspot.com",
+    messagingSenderId: "39898345407",
+    appId: "1:39898345407:web:7cdab41986edf28898bb0a"
+  }
+}
 
-const app = initializeApp(firebaseConfig);
+const isProd = () => (typeof import.meta !== 'undefined' && import.meta.env?.PROD)
+  || (globalThis as { process?: { env?: { NODE_ENV?: string } } }).process?.env?.NODE_ENV === 'production'
+
+const app = initializeApp(firebaseConfig[isProd() ? 'production' : 'staging']);
 export const db = initializeFirestore(app, {
   //localCache: persistentLocalCache({tabManager: persistentMultipleTabManager(), cacheSizeBytes: CACHE_SIZE_UNLIMITED}),
   ignoreUndefinedProperties: true,
